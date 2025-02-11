f1a9618786c90ff964f611c007f2ab54dfc4e037
gradeup-ai <basisconsulting2024@gmail.com>
2025-02-11 03:47:18 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
-(
-echo from fastapi import FastAPI
-echo.
-echo app = FastAPI()
-echo.
-echo @app.get("/")
-echo def read_root():
-echo     return {"message": "AI-HR API работает!"}
-) > app.py
- 
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "AI-HR API работает!"}
